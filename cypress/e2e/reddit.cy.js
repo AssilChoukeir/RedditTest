a61e6492ef7f74c88f51082bac019d394c752b3d
AssilChoukeir <assil.choukeir99@gmail.com>
2025-03-03 23:14:21 +0200

    Update reddit.cy.js

--- cypress/e2e/reddit.cy.js
+++ cypress/e2e/reddit.cy.js
@@ -5,27 +5,19 @@ describe('Reddit Test', () => {
 
       cy.get('#login-username').click();
       cy.focused().type('wmftcnjobtvwlwmeit@hthlm.com');
-
       cy.get('#login-password').click();
 
       cy.focused().type('P@ssw0rd..1');
-
       cy.focused().blur();
 
       cy.wait(500)
 
       cy.get('.login').click();
-
       cy.get('#search > [type="text"]', { timeout: 20000 }).should('exist');
-
       cy.get('#search > [type="text"]').type('r/gaming{enter}');
-
       cy.get('.search-result-header > .search-title > mark', { timeout: 20000 }).should('exist');
-
       cy.get('.search-result-header > .search-title > mark').first().click();
-
       cy.get('.odd', { timeout: 20000 }).should('exist');
-
       cy.get('.odd').eq(1).then(($el) => {
         if ($el.text().includes('Nintendo')) {
           cy.get('.arrow.up').eq(2).click().click();
